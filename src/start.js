75e0edb41d477357adf3bbc9a3af04af7f1486c6
Hammed Taofeek <htolajide@yahoo.com>
2020-06-10 13:25:33 +0100

    npm-rebuild

--- src/start.js
+++ src/start.js
@@ -2,6 +2,6 @@ const logger = require('simple-node-logger').createSimpleLogger();
 const app = require('./app');
 // store the port number
 // http://api.ipstack.com/197.210.53.116?access_key=c934a4c422466d14bb4cdcd82fa49547
-
+// "@babel/plugin-transform-runtime": "^7.9.6",
 const port = parseInt(process.env.PORT, 10) || 4500;
 app.listen(port, () => logger.info(`Zero hunger ready at ${port}`));
